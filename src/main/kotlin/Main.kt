294d2323196650c98f19864e0205b75b0c17404f
dominikrys <dominik.j.rys@gmail.com>
2020-06-24 15:38:46 +0100

    Add transactions between wallets to Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import ktcoin.*
 
-fun main(args: Array<String>) {
+fun main() {
+    // Create a blockchain and some wallets
     val blockChain = BlockChain()
     val wallet1 = Wallet.create(blockChain)
     val wallet2 = Wallet.create(blockChain)
@@ -8,20 +9,25 @@ fun main(args: Array<String>) {
     println("Wallet 1 balance: ${wallet1.balance}")
     println("Wallet 2 balance: ${wallet2.balance}")
 
+    // Give 100 credits to Wallet 1
     val tx1 = Transaction(sender = wallet1.publicKey, recipient = wallet1.publicKey, amount = 100)
     tx1.sign(wallet1.privateKey)
     tx1.outputs.add(TransactionOutput(recipient = wallet1.publicKey, amount = 100, transactionHash = tx1.hash))
 
-    val genesisBlock = Block(previousHash = "0")
+    // Add the transaction to a new block and add it to the blockchain
+    var genesisBlock = Block(previousHash = "0")
     genesisBlock.addTransaction(tx1)
-    blockChain.add(genesisBlock)
+    genesisBlock = blockChain.add(genesisBlock)
+
+    println("Blockchain isValid: ${blockChain.isValid()}")
+
+    // Transfer some credit from wallet 1 to wallet 2
+    val transferTx = wallet1.sendFundsTo(recipient = wallet2.publicKey, amountToSend = 50)
+    val secondBlock = Block(previousHash = genesisBlock.hash)
+    secondBlock.addTransaction(transferTx)
+    blockChain.add(secondBlock)
 
     println("Wallet 1 balance: ${wallet1.balance}")
     println("Wallet 2 balance: ${wallet2.balance}")
-
-    wallet1.sendFundsTo(recipient = wallet2.publicKey, amountToSend = 50)
-    //val secondBlock = blockChain.add(Block(genesisBlock.hash).addTransaction(tx2))
-
-    println("Wallet 1 balance: ${wallet1.balance}")
-    println("Wallet 2 balance: ${wallet2.balance}")
+    println("Blockchain isValid: ${blockChain.isValid()}")
 }
